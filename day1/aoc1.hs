6169f9350a47100d18644d8ceeae61ccb7386556
Guillaume DEREX <gderex8@orange.fr>
2022-12-02 17:19:23 +0100

    Day 1 improvement

--- day1/aoc1.hs
+++ day1/aoc1.hs
@@ -1,35 +1,28 @@
 import System.Environment
 import Data.List
+import Data.List.Split
 
-parse :: String -> Integer
+parse :: String -> Int
 parse line = if (length line) == 0
     then 0
     else read line
 
-toInts :: [String] -> [Integer]
+toInts :: [String] -> [Int]
 toInts lines = map (parse) lines
 
--- This isn't ideal, it sums all consecutive groups of numbers but sometimes starts midway
--- but it doesn't matter, since starting midway would only give a smaller number
--- and here we want the maximum
-squash :: [Integer] -> [Integer]
-squash [] = []
-squash scores = if (head scores /= 0)
-    then sum (takeWhile (\x -> x /= 0) scores) : squash (tail scores)
-    else squash (tail scores)
+squash :: [Int] -> [Int]
+squash scores = do
+    map (sum) (splitOn [0] scores) 
 
-bestSquash :: [Integer] -> Integer
-bestSquash scores = maximum (squash scores)
-
--- Actually it WOULD matter here but still, it got the right answer
-top3bestSquash :: [Integer] -> Integer
-top3bestSquash scores = sum (take 3 (sortOn (\x -> negate x) (squash scores)))
+top3best :: [Int] -> Int
+top3best squashes = sum $ take 3 (sortOn (\x -> negate x) squashes)
 
 main = do
    args <- getArgs
    content <- readFile (args !! 0)
    let linesOfFile = lines content
-   putStrLn (show (bestSquash (toInts linesOfFile)))
-   putStrLn (show (top3bestSquash (toInts linesOfFile)))
+   let squashes = squash (toInts linesOfFile)
+   putStrLn (show (maximum squashes))
+   putStrLn (show (top3best squashes))
 
 -- Guillaume DEREX
